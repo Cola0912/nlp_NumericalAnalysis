ef286506e1014bc1289cc4c1e0824a14dfbbe719
shusei Akiya <0912cocacola@gmail.com>
2022-11-28 01:25:24 +0900

    文字の種類追加

--- nlp.py
+++ nlp.py
@@ -1,7 +1,13 @@
 f = open('sample.txt', 'r', encoding='UTF-8')
 
 data = f.read()
-print(data)
-print(data.count('の'))
+print(data,'\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+print('の	',data.count('の'))
+print('ののの	',data.count('ののの'))
+print('!	',data.count('!'))
+print('2	',data.count('2'))
+print('sc	',data.count('sc'))
+print('&	',data.count('&'))
+
 
 f.close()
